2d4903d3f861b65ece8915cbab003835c73929bb
Danny Grove <danny@drgrovellc.com>
2014-10-03 18:50:49 -0700

    Adds example app and other starter files

---
+++ example/app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name exampleApp
+ * @description
+ * # exampleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('exampleApp', [
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
