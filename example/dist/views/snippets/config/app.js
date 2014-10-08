fe6e546bc5e3c849dd3836d850af3ae48b0b3970
Danny Grove <danny@drgrovellc.com>
2014-10-07 22:26:16 -0700

    Adds code snippets folder to dist

---
+++ example/dist/views/snippets/config/app.js
@@ -0,0 +1,18 @@
+'use strict'
+// Within the config function
+angular.module('youAppName',[]);
+angular.config(['ngHueConfig', function(ngHueConfig){
+  ngHueConfig.applicationName = 'yourAppName'
+  ngHueConfig.deviceType = 'browser'
+}])
+
+// Within the controller that has a form
+angular.controller('mainCtrl',['$scope', 'ngHueConfig',function($scope,ngHueConfig){
+  $scope.config = {}
+
+  $scope.updateConfig = function(){
+    for(var key in $scope.config) {
+      ngHueConfig[key] = $scope.config[key]
+    }
+  }
+}])
