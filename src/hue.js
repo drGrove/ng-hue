8af124ac6c9e9f426d3cb18e43f44d6ac2e2c987
Danny Grove <danny@drgrovellc.com>
2014-10-07 20:09:51 -0700

    Adds user creation

---
+++ src/hue.js
@@ -0,0 +1,84 @@
+(function(){
+  'use strict'
+
+  /**
+   * Defines angular module
+   * @name ngHue
+   */
+  angular
+  .module
+  ( 'ngHue'
+  , [
+    ]
+  )
+
+  /**
+   * ngHueConfig
+   * @constant
+   */
+  .constant
+  ( 'ngHueConfig'
+  , { deviceType: ''
+    , username: ''
+    , brideName: ''
+    , bridgeIP: ''
+    , applicationName: 'ngHue'
+    }
+  )
+
+  .directive
+  ( 'lightSelect'
+  , [
+    ]
+  )
+
+  .controller
+  ( 'lighSelectCtrl'
+  , [ '$scope'
+    , function
+      ( $scope
+      )
+      {
+
+      }
+    ]
+  )
+
+  .factory
+  ( 'HueConfiguration'
+  , [ '$http'
+    , '$q'
+    , 'ngHueConfig'
+    , function
+      ( $http
+      , $q
+      , ngHueConfig
+      )
+      {
+        var Configuration = {}
+        Configuration.user = {}
+        var baseUri = 'http://' + ngHueConfig.bridgeIP
+
+        Configuration.user.create = function(deviceType, username){
+          var endpoint = baseUri+ '/api'
+          var deferred = $q.defer()
+          var params = {}
+
+          params.deviceType = ngHueConfig.applicationName + '#' + deviceType
+
+          if(username)
+            params.username = username
+
+          $http.post(endpoint, params).then(function(res){
+            ngHueConfig.username = res.data.username
+            ngHueConfig.deviceType = deviceType
+            deferred.resolve(res)
+          })
+          return deferred.promise
+        }
+
+        return Configuration
+      }
+    ]
+  )
+}());
