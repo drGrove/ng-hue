6181f89338e105e185e1b7b73fd2879f15d51295
Danny Grove <danny@drgrovellc.com>
2014-10-07 22:41:21 -0700

    Adds toast factory

--- src/hue.js
+++ src/hue.js
@@ -11,7 +11,7 @@
   , [
     ]
   )
-
+  
   /**
    * ngHueConfig
    * @constant
@@ -39,11 +39,11 @@
       ( $scope
       )
       {
-
+      
       }
     ]
   )
-
+  
   .factory
   ( 'HueConfiguration'
   , [ '$http'
@@ -58,17 +58,17 @@
         var Configuration = {}
         Configuration.user = {}
         var baseUri = 'http://' + ngHueConfig.bridgeIP
-
+        
         Configuration.user.create = function(deviceType, username){
           var endpoint = baseUri+ '/api'
           var deferred = $q.defer()
           var params = {}
-
+          
           params.deviceType = ngHueConfig.applicationName + '#' + deviceType
-
+          
           if(username)
             params.username = username
-
+          
           $http.post(endpoint, params).then(function(res){
             ngHueConfig.username = res.data.username
             ngHueConfig.deviceType = deviceType
@@ -76,9 +76,32 @@
           })
           return deferred.promise
         }
-
+        
         return Configuration
       }
     ]
   )
+  
+  .factory
+  ( 'HueToast'
+  , [ function
+      (
+      )
+      {
+        var HueToast = {}
+        
+        HueToast.toasts = []
+         
+        HueToast.add = function(toast) {
+          HueToast.toasts.push(toast)
+        }
+        
+        HueToast.remove = function(idx) {
+          HueToast.toasts.splice(idx, 1)
+        }
+        
+        return HueToast
+      }
+    ] 
+    )
 }());
