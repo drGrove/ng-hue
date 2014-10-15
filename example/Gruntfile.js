71d8d2634955b48c70717ca3d6af60b32eea2863
Danny Grove <danny@drgrovellc.com>
2014-10-14 17:25:37 -0700

    Change to ng-annotate

--- example/Gruntfile.js
+++ example/Gruntfile.js
@@ -277,17 +277,21 @@ module.exports = function (grunt) {
       }
     },
 
-    // ngmin tries to make the code safe for minification automatically by
+    // ngAnnotate tries to make the code safe for minification automatically by
     // using the Angular long form for dependency injection. It doesn't work on
     // things like resolve or inject so those have to be done manually.
-    ngmin: {
-      dist: {
-        files: [{
-          expand: true,
-          cwd: '.tmp/concat/scripts',
-          src: '*.js',
-          dest: '.tmp/concat/scripts'
-        }]
+    ngAnnotate:
+    { dist:
+      { files:
+        [ { expand: true
+          , cwd: '.tmp/concat/scripts'
+          , src:
+            [ '*.js'
+            , '!oldieshim.js'
+            ]
+          , dest: '.tmp/concat/scripts'
+          }
+        ]
       }
     },
 
@@ -394,7 +398,7 @@ module.exports = function (grunt) {
     'concurrent:dist',
     'autoprefixer',
     'concat',
-    'ngmin',
+    'ngAnnotate',
     'copy:dist',
     'cdnify',
     'cssmin',
